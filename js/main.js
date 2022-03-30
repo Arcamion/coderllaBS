150a461831b97f3d74d5a1aa816b33e28663577f
tomas <tomasmiskolczi@gmail.com>
2022-03-30 12:02:29 -0300

    termino de los html y el index

--- js/main.js
+++ js/main.js
@@ -5,4 +5,16 @@ $(function() {
 
     // wow js
     new WOW().init();
+    // tinyslider js
+
+    
+    var slider = tns({
+        container: '.noticias-active',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        mouseDrag: true,
+        nav: false,
+        controlsText: ['<i class="fa-solid fa-angle-left prev"></i>', '<i class="fa-solid fa-angle-right next"></i>'],
+      });
 });
